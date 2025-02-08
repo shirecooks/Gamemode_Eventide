5060373c57e36e0e845914248da1a4e871512606
Luis <104567400+Surdavow@users.noreply.github.com>
2025-02-08 03:05:01 -0800

    Move CustomCDNServer package and to new support file

--- modules/scripts/module_scripts.cs
+++ modules/scripts/module_scripts.cs
@@ -12,6 +12,7 @@ exec("./support/support_stringutilities.cs");
 exec("./support/support_vectorutilities.cs");
 exec("./support/support_texttransfer.cs");
 exec("./support/support_billboards.cs");
+exec("./support/support_cdnoverride.cs");
 exec("./script_itemparticle.cs");
 exec("./script_itemspawning.cs");
 exec("./billboard_example.cs");
---
+++ modules/scripts/support/support_cdnoverride.cs
@@ -0,0 +1,34 @@
+package CustomCDNServer
+{
+	function GameConnection::onConnectRequest(%client, %netAddress, %LANname, %blid, %clanPrefix, %clanSuffix, %clientNonce,%g, %h, %i, %j, %k, %l, %m, %n, %o, %p)
+	{
+		%ret = Parent::onConnectRequest(%client, %netAddress, %LANname, %blid, %clanPrefix, %clanSuffix, %clientNonce,%g, %h, %i, %j, %k, %l, %m, %n, %o, %p);			
+		
+		%client.customCDN = false;
+
+		for(%cnt = 0; %cnt < getLineCount(%h); %cnt++)
+		{
+			%line = getLine(%h, %cnt);
+			if(getField(%line, 0) $= "CustomCDN")
+			{
+				%client.customCDN = true;
+				%client.customCDNUrl = $CustomCDN::CDN_to_clients;
+				%client.customCDNversion = getField(%line, 1);
+				break;
+			}
+		}
+        
+		if(!%client.customCDN)
+        {
+           return "<a:https://blocklandglass.com/addons/addon/1580>Support_CustomCDN</a> is required to play on this server";
+        }
+
+        return %ret;
+	}
+};
+
+if(isPackage(CustomCDNServer)) 
+{
+    deactivatePackage(CustomCDNServer);
+    activatePackage(CustomCDNServer);
+}
