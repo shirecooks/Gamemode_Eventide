3e4e204ac11db7243990ce75e00ff6e6c7e6b5e4
Private Caller <proforpenicillin@borrowedtime.online>
2025-02-09 13:34:31 -0600

    Support_CustomCDN client rejection enhancement.

--- modules/scripts/support/support_cdnoverride.cs
+++ modules/scripts/support/support_cdnoverride.cs
@@ -1,36 +1,14 @@
-package CustomCDNServer
+package Gamemode_Evenitde_CDN
 {
-	function GameConnection::onConnectRequest(%client, %netAddress, %LANname, %blid, %clanPrefix, %clanSuffix, %clientNonce,%g, %h, %i, %j, %k, %l, %m, %n, %o, %p)
+	function GameConnection::startLoad(%client)
 	{
-		%ret = Parent::onConnectRequest(%client, %netAddress, %LANname, %blid, %clanPrefix, %clanSuffix, %clientNonce,%g, %h, %i, %j, %k, %l, %m, %n, %o, %p);			
-		
-		%client.customCDN = false;
-
-		for(%cnt = 0; %cnt < getLineCount(%h); %cnt++)
-		{
-			%line = getLine(%h, %cnt);
-			if(getField(%line, 0) $= "CustomCDN")
-			{
-				%client.customCDN = true;
-				%client.customCDNUrl = $CustomCDN::CDN_to_clients;
-				%client.customCDNversion = getField(%line, 1);
-				break;
-			}
-		}
-        
 		if(!%client.customCDN)
-        {
-           warn("Client" SPC %client.getBLID() SPC "is missing CustomCDN, refusing connection");
-		   talk("Client" SPC %client.getBLID() SPC "is missing CustomCDN, everyone point and laugh at them XD");
-		   return "<a:https://blocklandglass.com/addons/addon/1580>Support_CustomCDN</a> is required to play on this server";
-        }
-
-        return %ret;
+		{
+			%client.delete("<a:https://blocklandglass.com/addons/addon/1580>Support_CustomCDN</a> is required to play on this server.");
+			announce(%client.getPlayerName() SPC "is missing CustomCDN, everyone point and laugh at them XD");
+			serverPlay2D("clown_horn_sound");
+		}
+		parent::startLoad(%client);
 	}
 };
-
-if(isPackage(CustomCDNServer)) 
-{
-    deactivatePackage(CustomCDNServer);
-    activatePackage(CustomCDNServer);
-}
+activatePackage(Gamemode_Evenitde_CDN);
