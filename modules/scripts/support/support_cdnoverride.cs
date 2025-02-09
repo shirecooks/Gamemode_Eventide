744fa931b7414c57073229b1a0d424a3e8daa0ba
Private Caller <proforpenicillin@borrowedtime.online>
2025-02-09 15:07:50 -0600

    Support_CustomCDN client rejection - Slayer error fix.

--- modules/scripts/support/support_cdnoverride.cs
+++ modules/scripts/support/support_cdnoverride.cs
@@ -1,14 +1,14 @@
-package Gamemode_Evenitde_CDN
+package Gamemode_Evenitde_CDN_Reject
 {
-	function GameConnection::startLoad(%client)
+	function serverCmdMissionStartPhase1Ack(%client, %seq)
 	{
+		parent::serverCmdMissionStartPhase1Ack(%client, %seq);
 		if(!%client.customCDN)
 		{
-			%client.delete("<a:https://blocklandglass.com/addons/addon/1580>Support_CustomCDN</a> is required to play on this server.");
 			announce(%client.getPlayerName() SPC "is missing CustomCDN, everyone point and laugh at them XD");
 			serverPlay2D("clown_horn_sound");
+			%client.delete("<a:https://blocklandglass.com/addons/addon/1580>Support_CustomCDN</a> is required to play on this server.");
 		}
-		parent::startLoad(%client);
 	}
 };
-activatePackage(Gamemode_Evenitde_CDN);
+activatePackage(Gamemode_Evenitde_CDN_Reject);
