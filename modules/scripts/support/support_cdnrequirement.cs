27f1cdc345b77650c4553546bbf96dccdf82db0c
Luis <104567400+Surdavow@users.noreply.github.com>
2025-02-10 00:22:18 -0800

    Remove humorous announcements for missing CustomCDN requirement

--- modules/scripts/support/support_cdnrequirement.cs
+++ modules/scripts/support/support_cdnrequirement.cs
@@ -5,8 +5,6 @@ package Gamemode_Eventide_CDN_Reject
 		parent::serverCmdMissionStartPhase1Ack(%client, %seq);
 		if(!%client.customCDN)
 		{
-			announce(%client.getPlayerName() SPC "is missing CustomCDN, everyone point and laugh at them XD");
-			serverPlay2D("clown_horn_sound");
 			%client.delete("<a:https://blocklandglass.com/addons/addon/1580>Support_CustomCDN</a> is required to play this server");
 		}
 	}
