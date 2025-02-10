cf40122a78a67b1b13ff78357dbeae425430ea9b
Luis <104567400+Surdavow@users.noreply.github.com>
2025-02-09 20:16:16 -0800

    Update Eventide billboard mount height for improved visibility

--- modules/scripts/script_billboards.cs
+++ modules/scripts/script_billboards.cs
@@ -165,4 +165,4 @@ package MountGroup_Billboards
 };
 activatePackage(MountGroup_Billboards);
 
-$Eventide::BillboardMounts = MountGroup_Create(OverheadBillboardMount,16,5);
+$Eventide::BillboardMounts = MountGroup_Create(OverheadBillboardMount,16,7);
