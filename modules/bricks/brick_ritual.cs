0479aec668556ba6cf845cce791a88479ef671a9
Luis <104567400+Surdavow@users.noreply.github.com>
2025-02-09 21:04:53 -0800

    Debug test

--- modules/bricks/brick_ritual.cs
+++ modules/bricks/brick_ritual.cs
@@ -96,6 +96,8 @@ function brickEventideRitual::ritualCheck(%this,%obj)
 	}
 	else %eventideminigame = 0;
 
+	talk(%eventideminigame);
+
 	if(%obj.ritualsPlaced < 10 && isObject(%eventideminigame))
 	{
 		initContainerRadiusSearch(%obj.getPosition(), 2.5, $TypeMasks::ItemObjectType | $TypeMasks::PlayerObjectType);		
