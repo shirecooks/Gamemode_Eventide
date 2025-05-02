5081c7d58d7cf877162d59db061c9d00146a4eff
PrivateCaller <proforpenicillin@borrowedtime.online>
2025-05-02 15:40:50 -0500

    .OGG auto-loading support.

--- modules/misc/datablocks_sound.cs
+++ modules/misc/datablocks_sound.cs
@@ -18,82 +18,87 @@ datablock AudioDescription(AudioFSWalk)
 	type = $SimAudioType;
 };
 
-%pattern = "./*.wav";
-%file = findFirstFile(%pattern);
-while (%file !$= "") 
+%patterns = ".wav\t.ogg";
+for(%i = 0; %i < getFieldCount(%patterns); %i++)
 {
-    %soundName = strreplace(filename(strlwr(%file)), ".wav", "");
+    %pattern = getField(%patterns, %i);
 
-    // Automatic sound instancing based on name of the sound
-    if (strstr(%file, "normal") != -1) 
-	{
-        %description = "AudioClose3d";
-
-        if (strstr(%file, "loop") != -1) 
-	    {
-            %description = "AudioCloseLooping3d";
-        }        
-    } 
-	else if (strstr(%file, "quiet") != -1) 
-	{
-        %description = "AudioClosest3d";
-
-        if (strstr(%file, "loop") != -1) 
-	    {
-            %description = "AudioClosestLooping3d";
-        }
-    } 
-	else if (strstr(%file, "loud") != -1) 
-	{
-        %description = "AudioDefault3d";
-
-        if (strstr(%file, "loop") != -1) 
-	    {
-            %description = "AudioDefaultLooping3d";
-        }
-    }
-    else if (strstr(%file, "music") != -1) 
-	{
-        %soundName = fileBase(%file);
-        %description = "AudioMusicLooping3d";
-    }
-	else 
-	{
-        %description = ""; // No match, do not create datablock
-    }
-
-    // Special handling for footsteps
-    if (strstr(%file, "sounds/footsteps/") != -1) 
-	{
-        if (strstr(%file, "walk") != -1 || strstr(%file, "swim") != -1) 
-		{
-            %description = "AudioFSWalk";
-        } 
-		else if (strstr(%file, "run") != -1) 
-		{
-            %description = "AudioFSRun";
-        } 
-		else 
-		{
-            %description = ""; // No match for footsteps, skip
-        }
-    }
-
-    // Skip if no valid description
-    if (%description $= "") 
-	{
-        %file = findNextFile(%pattern);
-        continue;
-    }
-
-    // Create AudioProfile datablock
-    if(%description $= "AudioMusicLooping3d")
+    %file = findFirstFile("./*" @ %pattern);
+    while (%file !$= "") 
     {
-        eval("datablock AudioProfile(musicData_" @ %soundName @ ") { preload = true; description = " @ %description @ "; filename = \"" @ %file @ "\"; uiName=\"" @ %soundName @ "\"; };");
+        %soundName = strreplace(filename(strlwr(%file)), %pattern, "");
+
+        // Automatic sound instancing based on name of the sound
+        if (strstr(%file, "normal") != -1) 
+        {
+            %description = "AudioClose3d";
+
+            if (strstr(%file, "loop") != -1) 
+            {
+                %description = "AudioCloseLooping3d";
+            }        
+        } 
+        else if (strstr(%file, "quiet") != -1) 
+        {
+            %description = "AudioClosest3d";
+
+            if (strstr(%file, "loop") != -1) 
+            {
+                %description = "AudioClosestLooping3d";
+            }
+        } 
+        else if (strstr(%file, "loud") != -1) 
+        {
+            %description = "AudioDefault3d";
+
+            if (strstr(%file, "loop") != -1) 
+            {
+                %description = "AudioDefaultLooping3d";
+            }
+        }
+        else if (strstr(%file, "music") != -1) 
+        {
+            %soundName = fileBase(%file);
+            %description = "AudioMusicLooping3d";
+        }
+        else 
+        {
+            %description = ""; // No match, do not create datablock
+        }
+
+        // Special handling for footsteps
+        if (strstr(%file, "sounds/footsteps/") != -1) 
+        {
+            if (strstr(%file, "walk") != -1 || strstr(%file, "swim") != -1) 
+            {
+                %description = "AudioFSWalk";
+            } 
+            else if (strstr(%file, "run") != -1) 
+            {
+                %description = "AudioFSRun";
+            } 
+            else 
+            {
+                %description = ""; // No match for footsteps, skip
+            }
+        }
+
+        // Skip if no valid description
+        if (%description $= "") 
+        {
+            %file = findNextFile("./*" @ %pattern);
+            continue;
+        }
+
+        // Create AudioProfile datablock
+        if(%description $= "AudioMusicLooping3d")
+        {
+            eval("datablock AudioProfile(musicData_" @ %soundName @ ") { preload = true; description = " @ %description @ "; filename = \"" @ %file @ "\"; uiName=\"" @ %soundName @ "\"; };");
+        }
+        else
+        {
+            eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = " @ %description @ "; filename = \"" @ %file @ "\"; };");
+        }
+        %file = findNextFile("./*" @ %pattern);
     }
-    else
-    {
-        eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = " @ %description @ "; filename = \"" @ %file @ "\"; };");
-    }
-    %file = findNextFile(%pattern);
 }
--- modules/players/datablocks_sound.cs
+++ modules/players/datablocks_sound.cs
@@ -1,77 +1,85 @@
 // Create the sound datablocks
-%pattern = "./*.wav";
-%file = findFirstFile(%pattern);
-while (%file !$= "") 
+%patterns = ".wav\t.ogg";
+for(%i = 0; %i < getFieldCount(%patterns); %i++)
 {
-    %soundName = strreplace(filename(strlwr(%file)), ".wav", "");
+    %pattern = getField(%patterns, %i);
 
-    // Automatic sound instancing based on name of the sound
-    if (strstr(%file, "normal") != -1) 
-	{
-        %description = "AudioClose3d";
-    } 
-	else if (strstr(%file, "quiet") != -1) 
-	{
-        %description = "AudioClosest3d";
-    } 
-	else if (strstr(%file, "loud") != -1) 
-	{
-        %description = "AudioDefault3d";
-    }
-    else if (strstr(%file, "normal_loop") != -1) 
-	{
-        %description = "AudioCloseLooping3d";
-    }
-    else if (strstr(%file, "quiet_loop") != -1) 
-	{
-        %description = "AudioClosestLooping3d";
-    } 
-    else if (strstr(%file, "loud_loop") != -1) 
-	{
-        %description = "AudioDefaultLooping3d";
-    }
-    else if (strstr(%file, "music") != -1) 
-	{
-        %soundName = fileBase(%file);
-        %description = "AudioMusicLooping3d";
-    }
-	else 
-	{
-        %description = ""; // No match, do not create datablock
-    }
+    %file = findFirstFile("./*" @ %pattern);
+    while (%file !$= "") 
+    {
+        %soundName = strreplace(filename(strlwr(%file)), %pattern, "");
 
-    // Special handling for footsteps
-    if (strstr(%file, "sounds/footsteps/") != -1) 
-	{
-        if (strstr(%file, "walk") != -1 || strstr(%file, "swim") != -1) 
-		{
-            %description = "AudioFSWalk";
+        // Automatic sound instancing based on name of the sound
+        if (strstr(%file, "normal") != -1) 
+        {
+            %description = "AudioClose3d";
+
+            if (strstr(%file, "loop") != -1) 
+            {
+                %description = "AudioCloseLooping3d";
+            }        
         } 
-		else if (strstr(%file, "run") != -1) 
-		{
-            %description = "AudioFSRun";
+        else if (strstr(%file, "quiet") != -1) 
+        {
+            %description = "AudioClosest3d";
+
+            if (strstr(%file, "loop") != -1) 
+            {
+                %description = "AudioClosestLooping3d";
+            }
         } 
-		else 
-		{
-            %description = ""; // No match for footsteps, skip
+        else if (strstr(%file, "loud") != -1) 
+        {
+            %description = "AudioDefault3d";
+
+            if (strstr(%file, "loop") != -1) 
+            {
+                %description = "AudioDefaultLooping3d";
+            }
         }
-    }
+        else if (strstr(%file, "music") != -1) 
+        {
+            %soundName = fileBase(%file);
+            %description = "AudioMusicLooping3d";
+        }
+        else 
+        {
+            %description = ""; // No match, do not create datablock
+        }
 
-    // Skip if no valid description
-    if (%description $= "") 
-	{
-        %file = findNextFile(%pattern);
-        continue;
-    }
+        // Special handling for footsteps
+        if (strstr(%file, "sounds/footsteps/") != -1) 
+        {
+            if (strstr(%file, "walk") != -1 || strstr(%file, "swim") != -1) 
+            {
+                %description = "AudioFSWalk";
+            } 
+            else if (strstr(%file, "run") != -1) 
+            {
+                %description = "AudioFSRun";
+            } 
+            else 
+            {
+                %description = ""; // No match for footsteps, skip
+            }
+        }
 
-    // Create AudioProfile datablock
-    if(%description $= "AudioMusicLooping3d")
-    {
-        eval("datablock AudioProfile(musicData_" @ %soundName @ ") { preload = true; description = " @ %description @ "; filename = \"" @ %file @ "\"; uiName=\"" @ %soundName @ "\"; };");
+        // Skip if no valid description
+        if (%description $= "") 
+        {
+            %file = findNextFile("./*" @ %pattern);
+            continue;
+        }
+
+        // Create AudioProfile datablock
+        if(%description $= "AudioMusicLooping3d")
+        {
+            eval("datablock AudioProfile(musicData_" @ %soundName @ ") { preload = true; description = " @ %description @ "; filename = \"" @ %file @ "\"; uiName=\"" @ %soundName @ "\"; };");
+        }
+        else
+        {
+            eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = " @ %description @ "; filename = \"" @ %file @ "\"; };");
+        }
+        %file = findNextFile("./*" @ %pattern);
     }
-    else
-    {
-        eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = " @ %description @ "; filename = \"" @ %file @ "\"; };");
-    }
-    %file = findNextFile(%pattern);
 }
