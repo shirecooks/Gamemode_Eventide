6760da44b993cdb71d4704c7b6a64bff3cb0dce0
PrivateCaller <proforpenicillin@borrowedtime.online>
2025-07-24 17:08:54 -0500

    Fighter - initial commit.

---
+++ players/player_fighter.cs
@@ -0,0 +1,42 @@
+//
+// Core and appearance.
+//
+
+datablock PlayerData(PlayerFighter : PlayerSurvivor)
+{
+    class = "PlayerFighter";
+    superClass = "PlayerSurvivor";
+
+    uiName = "Fighter";
+    maxDamage = 175;
+    shoveForce = 2;
+};
+//Inherits functions from `PlayerSurvivor`.
+PlayerFighter.inheritFunctionsFromSuperClass();
+
+function PlayerFighter::onNewDatablock(%this, %obj)
+{
+	%client = %obj.client;
+	%clientExists = isObject(%client);
+
+	//Face pack initialization, very basic for now. 
+	//This can't use the usual datablock field method, since it needs to be dynamic.
+	%facePack = (%clientExists && %client.chest) ? $Eventide_FacePacks["fighterF"] : $Eventide_FacePacks["fighterM"];
+	%obj.createFaceConfig(%facePack);
+
+	//Voice Pack initialization.
+	%voicePack = (%clientExists && %client.chest) ? $Eventide_VoicePacks["female"] : $Eventide_VoicePacks["male"];
+	%obj.createVoiceConfig(%voicePack);
+
+	//Store some information used for voice-lines and chase management.
+	%obj.chasingKillers = new SimSet();
+	%obj.nearbyKillers = new SimSet();
+}
+
+function PlayerFighter::eventideBodyParts(%this, %obj)
+{
+	%this.super("eventideBodyParts", %this, %obj);
+
+    //Give their torso a custom decal.
+	%obj.setDecalName("francis");
+}
--- server.cs
+++ server.cs
@@ -15,6 +15,8 @@ exec("./players/datablock_killerMeleeProp.cs");
 exec("./players/player_eventide.cs");
 exec("./players/player_survivor.cs");
 exec("./players/player_staller.cs");
+exec("./players/player_tinkerer.cs");
+exec("./players/player_fighter.cs");
 exec("./players/player_killer.cs");
 exec("./players/player_renowned.cs");
 
