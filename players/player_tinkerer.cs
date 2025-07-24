3b815b0bb5b3ac244124dcbd226bbb9d987d0f91
PrivateCaller <proforpenicillin@borrowedtime.online>
2025-07-24 16:44:40 -0500

    Tinkerer - initial commit.

---
+++ players/player_tinkerer.cs
@@ -0,0 +1,40 @@
+//
+// Core and appearance.
+//
+
+datablock PlayerData(PlayerTinkerer : PlayerSurvivor)
+{
+    class = "PlayerTinkerer";
+    superClass = "PlayerSurvivor";
+
+    uiName = "Tinkerer";
+};
+//Inherits functions from `PlayerSurvivor`.
+PlayerTinkerer.inheritFunctionsFromSuperClass();
+
+function PlayerTinkerer::onNewDatablock(%this, %obj)
+{
+	%client = %obj.client;
+	%clientExists = isObject(%client);
+
+	//Face pack initialization, very basic for now. 
+	//This can't use the usual datablock field method, since it needs to be dynamic.
+	%facePack = (%clientExists && %client.chest) ? $Eventide_FacePacks["tinkererF"] : $Eventide_FacePacks["tinkererM"];
+	%obj.createFaceConfig(%facePack);
+
+	//Voice Pack initialization.
+	%voicePack = (%clientExists && %client.chest) ? $Eventide_VoicePacks["female"] : $Eventide_VoicePacks["male"];
+	%obj.createVoiceConfig(%voicePack);
+
+	//Store some information used for voice-lines and chase management.
+	%obj.chasingKillers = new SimSet();
+	%obj.nearbyKillers = new SimSet();
+}
+
+function PlayerTinkerer::eventideBodyParts(%this, %obj)
+{
+	%this.super("eventideBodyParts", %this, %obj);
+
+    //Give their torso a custom decal.
+	%obj.setDecalName("civilian");
+}
