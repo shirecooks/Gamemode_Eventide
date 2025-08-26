793091797570a0b61b8bcdfb7ee5cf16c672eedf
PrivateCaller <proforpenicillin@borrowedtime.online>
2025-08-25 23:07:10 -0500

    Killer melees - weapon state optimization and dead code removal.

--- players/datablock_killerMeleeProp.cs
+++ players/datablock_killerMeleeProp.cs
@@ -28,65 +28,27 @@ datablock ShapeBaseImageData(KillerMeleeImage)
    	ammo = "";
    	projectile = "";
    	projectileType = Projectile;
-   	melee = false;
-   	armReady = true;
+   	melee = true;
+   	armReady = false;
    	doColorShift = false;
 
 	stateName[0] = "Activate";
-	stateTimeoutValue[0] = 1;
+	stateWaitForTimeout[0] = true;
+	stateTimeoutValue[0] = 0.01;
 	stateTransitionOnTimeout[0] = "Ready";
-	stateScript[0] = "onAim";
-	stateSound[0] = weaponSwitchSound;
 
 	stateName[1] = "Ready";
-	stateTimeoutValue[1] = 3;
-	stateTransitionOnTimeout[1] = "ReadyDown";
-	stateTransitionOnTriggerDown[1]  = "Fire";
-	stateWaitForTimeout[1] = false;
+	stateTransitionOnTriggerDown[1]  = "Swing";
 	stateAllowImageChange[1] = true;
 	stateSequence[1] = "Ready";
 
-	stateName[7] = "ReadyDown";
-	stateSound[7] = weaponSwitchSound;
-	stateTransitionOnTriggerDown[7] = "AimBeat";
-	stateAllowImageChange[7] = true;
-	stateScript[7] = "onDrop";
-	stateSequence[7] = "Ready";
-
-	stateName[2] = "Fire";
-	stateTransitionOnTimeout[2] = "Smoke";
-	stateTimeoutValue[2] = 0.15;
-	stateScript[2] = "onFireDown";
+	stateName[2] = "Swing";
+	stateScript[2] = "onSwing";
+	stateFire[2] = false;
+	stateAllowImageChange[2] = false;
 	stateWaitForTimeout[2] = true;
-   	stateSequence[2] = "Fire";
-
-	stateName[3] = "Smoke";
-	stateScript[3] = "onSwing";
-	stateFire[3] = true;
-	stateAllowImageChange[3] = false;
-	stateSequence[3] = "Fire";
-	stateTimeoutValue[3] = 0.15;
-	stateSound[3] = "";
-	stateTransitionOnTimeout[3] = "CoolDown";
-
-   	stateName[5] = "CoolDown";
-   	stateTimeoutValue[5] = 0.9;
-	stateTransitionOnTimeout[5] = "Reload";
-   	stateSequence[5] = "clickDown";
-
-	stateName[4] = "Reload";
-	stateTransitionOnTriggerUp[4] = "Ready";
-	stateSequence[4] = "TrigDown";
-
-   	stateName[6] = "NoAmmo";
-   	stateTransitionOnAmmo[6] = "Ready";
-
-	stateName[8] = "AimBeat";
-	stateTimeoutValue[8] = 0.05;
-	stateTransitionOnTimeout[8] = "Fire";
-	stateAllowImageChange[8] = true;
-	stateScript[8] = "onAim";
-	stateSequence[8] = "clickDown";
+	stateTimeoutValue[2] = 0.15;
+	stateTransitionOnTimeout[2] = "Ready";
 };
 
 function KillerMeleeImage::onSwing(%this, %obj, %slot)
