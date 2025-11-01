d3d4d35af8b816e86abcd5f6ed723098224fb1d7
PrivateCaller <proforpenicillin@borrowedtime.online>
2025-11-01 01:44:13 -0500

    Object collision - bugfix, no longer collide with self.

--- scripts/support_objectCollision.cs
+++ scripts/support_objectCollision.cs
@@ -1,5 +1,10 @@
 function onObjectCollisionTest(%obj, %col)
 {
+    if(%obj == %col)
+    {
+        return true;
+    }
+
     %hostDatablock = %obj.Datablock;
     %targetDatablock = %col.Datablock;
 
