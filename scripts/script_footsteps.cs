b2d2d1e61638ddc6335fc013f37c3f42df4e1e7e
PrivateCaller <proforpenicillin@borrowedtime.online>
2025-11-03 00:46:42 -0600

    Footsteps system - progress.

--- scripts/script_footsteps.cs
+++ scripts/script_footsteps.cs
@@ -5,17 +5,35 @@
 $Eventide_FootstepMaterials["isGlobalFootstepMaterialArray"] = true; //All footstep packs will be stored in this array.
 $Eventide_FootstepMaterials["index"] = ""; //Footsteps will be happening too often for us to use a ScriptObject. We'll instead be using a plain array.
 
-function createFootstepMaterial(%footstepFileCategory, %footstepMaterialRGB)
+function createFootstepMaterial(%footstepMaterialFile, %footstepMaterialRGB)
 {
-	//Add the footstep pack RGB value to the index, to be iterated over later.
-	$Eventide_FootstepsPacks["index"] = ($Eventide_FootstepsPacks["index"] $= "") ? (%footstepMaterialRGB) : ($Eventide_FootstepsPacks["index"] SPC %footstepMaterialRGB);
+	%material = fileBase(%footstepMaterialFile);
+	%cwd = filePath(%footstepMaterialFile);
 
-	$Eventide_FootstepsPacks[%footstepMaterialRGB] = %footstepFileCategory;
+	//Count the number of sounds available for a given material.
+	%soundEffectCount = 0;
+	%patterns = ".wav\t.ogg";
+	for(%i = 0; %i < getFieldCount(%patterns); %i++)
+	{
+		%pattern = getField(%patterns, %i);
+		for(%file = findFirstFile(%cwd @ "/*" @ %pattern); %file !$= ""; %file = findNextFile(%cwd @ "/*" @ %pattern))
+		{
+			%soundEffectCount++;
+		}
+	}
+
+	//Add the footstep pack RGB value to the index, to be iterated over later.
+	$Eventide_FootstepMaterials["index"] = ($Eventide_FootstepMaterials["index"] $= "") ? (%footstepMaterialRGB) : ($Eventide_FootstepMaterials["index"] SPC %footstepMaterialRGB);
+	
+	$Eventide_FootstepMaterials[%material] = %footstepMaterialRGB;
+	$Eventide_FootstepMaterials[%footstepMaterialRGB] = %material;
+	$Eventide_FootstepMaterials[%footstepMaterialRGB, "count"] = %soundEffectCount;
 }
 
 function parseFootstepMaterials(%startingDirectory)
 {    
-    %footstepMaterialPaths = getFileString(%startingDirectory @ "/*.etfsp");
+    %footstepMaterialPaths = getFileString(%startingDirectory @ "/*.etmp");
+	echo("Footstep material file string:" SPC %footstepMaterialPaths);
     for(%i = 0; %i < getFieldCount(%footstepMaterialPaths); %i++)
     {
 		%footstepMaterialFile = getField(%footstepMaterialPaths, %i);
@@ -28,7 +46,7 @@ function parseFootstepMaterials(%startingDirectory)
         %footstepMaterialFileName = fileBase(%footstepMaterialPath);
 
         echo("Parsing footstep material \"" @ %footstepMaterialFileName @ "\" from \"" @ %footstepMaterialPath @ "\"...");
-        %footstepMaterial = createFootstepMaterial(%footstepMaterialFileName, %footstepMaterialRGB);
+        %footstepMaterial = createFootstepMaterial(%footstepMaterialFile, %footstepMaterialRGB);
     }
 }
 
@@ -43,9 +61,11 @@ function findClosestFootstepMaterial(%rgb)
 
 	//Determine which material has the closest RGB similarity to the provided RGB color.
 	%materialIndex = $Eventide_FootstepMaterials["index"];
+	talk("Index:" SPC %materialIndex);
 	for(%i = 0; %i < getWordCount(%materialIndex); %i++)
 	{
 		%targetRGB = getWord(%materialIndex, %i);
+		talk("Candidate RGB:" SPC %targetRGB);
 		%euclideanColorDistance = VectorDist(%rgb, %targetRGB);
 
 		if(%euclideanColorDistance < %currentDistance)
@@ -55,7 +75,22 @@ function findClosestFootstepMaterial(%rgb)
 		}
 	}
 
+	talk("Closest RGB value found:" SPC %closestRGB);
+
 	return $Eventide_FootstepMaterials[%closestRGB];
+}
+
+function getFootstepSoundFromMaterial(%material)
+{
+	%rgb = $Eventide_FootstepMaterials[%material];
+	if(%rgb $= "")
+	{
+		//No sound effect found.
+		return -1;
+	}
+
+	%soundEffectCount = $Eventide_FootstepMaterials[%rgb, "count"];
+	return "fs" @ %material @ getRandom(1, %soundEffectCount) @ "_sound";
 }
 
 package Script_Footsteps
@@ -140,7 +175,7 @@ function Armor::getNextFootstepTime(%this, %obj)
 	%maxStepsPerSecond = 3.5;
 	%runTransitionPoint = 6.0;
 	%cadenceRise = 0.35;
-	return (%maxStepsPerSecond / (1.0 + mPow(%e, (-%k * (%relevantSpeed - %runTransitionPoint)))));
+	return 1000 * (%maxStepsPerSecond / (1.0 + mPow(%e, (-%k * (%relevantSpeed - %runTransitionPoint)))));
 }
 
 function Armor::getFootstepSound(%this, %obj)
@@ -149,6 +184,12 @@ function Armor::getFootstepSound(%this, %obj)
 	{
 		//If the player is in a vehicle, no footsteps.
 		return -1;
+	}
+
+	if(%obj.getWaterCoverage() >= 0.4)
+	{
+		//The player is swimming, get some splash sounds.
+		return getFootstepSoundFromMaterial("water");
 	}
 
 	%playerPosition = %obj.getPosition();
@@ -161,12 +202,74 @@ function Armor::getFootstepSound(%this, %obj)
 		return -1;
 	}
 
-	%collider = containerRayCast(%playerPosition, VectorAdd(%playerPosition, "0.0 0.0 -0.1"), %typemask);
+	%collider = containerRayCast(VectorAdd(%playerPosition, "0.0 0.0 0.1"), VectorAdd(%playerPosition, "0.0 0.0 -0.1"), %typemask);
 	%colliderType = %collider.getType();
-	if(%colliderType & $TypeMasks::fxBrickObjectType || %colliderType & $TypeMasks::VehicleObjectType)
+	if(!(%colliderType & $TypeMasks::FxBrickObjectType) && !(%colliderType & $TypeMasks::VehicleObjectType))
 	{
-		//For bricks, we only really have color to go on for assuming material.
-		//For vehicles, we can assume metal if there's wheels, and perhaps wood otherwise (like a boat.)
-		return %collider.assumeMaterial();
+		return;
 	}
+
+	//For bricks, we only really have color to go on for assuming material.
+	//For vehicles, we can assume metal if there's wheels, and perhaps wood otherwise (like a boat.)
+	//For everything else, just assume tile.
+	%material = %collider.assumeMaterial();
+	return getFootstepSoundFromMaterial(%material);
 }
+
+//
+// Loop and initialization.
+//
+
+function Armor::footstepTick(%this, %obj)
+{
+	if(!isObject(%obj) || %obj.getState() $= "Dead")
+	{
+		return;
+	}
+
+	%playerDatablock = %obj.getDatablock();
+	%nextFootstepTime = %playerDatablock.getNextFootstepTime(%obj);
+
+	if(%nextFootstepTime != -1)
+	{
+		%soundDatablock = %playerDatablock.getFootstepSound(%obj);
+		if(%soundDatablock != -1)
+		{
+			ServerPlay3D(%soundDatablock, %obj.getPosition());
+		}
+	}
+	else
+	{
+		%nextFootstepTime = 33;
+	}
+
+	cancel(%obj.footstepSchedule);
+	%obj.footstepSchedule = %this.schedule(%nextFootstepTime, "footstepTick", %obj);
+}
+
+package Script_Footsteps
+{
+	function Armor::onAdd(%this, %obj)
+	{
+		parent::onAdd(%this, %obj);
+		cancel(%obj.footstepSchedule);
+		%this.footstepTick(%obj);
+	}
+
+	function Armor::onDisabled(%this, %obj)
+	{
+		parent::onDisabled(%this, %obj);
+		cancel(%obj.footstepSchedule);
+	}
+
+	function Armor::onRemove(%this, %obj)
+	{
+		parent::onRemove(%this, %obj);
+		cancel(%obj.footstepSchedule);
+	}
+};
+if(isPackage(Script_Footsteps))
+{
+	deactivatePackage(Script_Footsteps);
+}
+activatePackage(Script_Footsteps);
