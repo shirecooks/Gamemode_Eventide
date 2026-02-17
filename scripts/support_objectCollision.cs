8ff4aa9933dee23de2f247428b69ca8ae1013533
PrivateCaller <proforpenicillin@borrowedtime.online>
2026-02-16 22:56:17 -0600

    Object collision - caching optimization.

--- scripts/support_objectCollision.cs
+++ scripts/support_objectCollision.cs
@@ -1,3 +1,7 @@
+//
+// Returning true causes a collision, false prevents it.
+// Return true by default.
+//
 function onObjectCollisionTest(%obj, %col)
 {
     if(%obj == %col)
@@ -8,18 +12,33 @@ function onObjectCollisionTest(%obj, %col)
     %hostDatablock = %obj.Datablock;
     %targetDatablock = %col.Datablock;
 
-    %hostCollisionHandler = isFunction(%hostDatablock, onObjectCollision);
-    %targetCollisionHandler = isFunction(%targetDatablock, onObjectCollision);
+    //Check if the host has custom collision handling. If so, cache the result for more performance next time.
+    %hostCustomCollision = %hostDatablock.handlesCollision;
+    if(%hostDatablock.handlesCollision $= "")
+    {
+        %hostDatablock.handlesCollision = isFunction(%hostDatablock, onObjectCollision);
+        %hostCustomCollision = %hostDatablock.handlesCollision;
+    }
 
-	if(%hostCollisionHandler && %targetCollisionHandler)
+    //Do the same for the target object.
+    %targetCustomCollision = %targetDatablock.handlesCollision;
+    if(%targetDatablock.handlesCollision $= "")
+    {
+        %targetDatablock.handlesCollision = isFunction(%targetDatablock, onObjectCollision);
+        %targetCustomCollision = %targetDatablock.handlesCollision;
+    }
+
+    //Based on which object has custom collision handling, call the appropriate functions.
+    //If BOTH have custom collision handling, aggregate both results. Both must agree to allow the collision to occur.
+	if(%hostCustomCollision && %targetCustomCollision)
     {
         return (%hostDatablock.onObjectCollision(%obj, %col) && %targetDatablock.onObjectCollision(%col, %obj));
     }
-    else if(%hostCollisionHandler)
+    else if(%hostCustomCollision)
     {
         return %hostDatablock.onObjectCollision(%obj, %col);
     }
-    else if(%targetCollisionHandler)
+    else if(%targetCustomCollision)
     {
         return %targetDatablock.onObjectCollision(%col, %obj);
     }
