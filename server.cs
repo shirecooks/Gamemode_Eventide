4d49110aedb41becd1283f4dfbabad605a88de1f
PrivateCaller <proforpenicillin@borrowedtime.online>
2026-02-18 01:35:02 -0600

    `GVarAccess` integration - initial commit.

--- server.cs
+++ server.cs
@@ -1,5 +1,6 @@
 $Eventide_BaseDirectory = filePath($Con::File);
 
+exec("./scripts/support_globalVarAccess.cs");
 exec("./scripts/support_oop.cs");
 exec("./scripts/support_common.cs");
 exec("./scripts/support_imageAttributes.cs");
