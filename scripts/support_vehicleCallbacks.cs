4814059c4e555f4cdc414afd0bd137afa5ab4d5a
PrivateCaller <proforpenicillin@borrowedtime.online>
2026-09-07 20:55:40 -0500

    Vehicle callbacks - added support for unpushable vehicles via `cantPush` field.

--- scripts/support_vehicleCallbacks.cs
+++ scripts/support_vehicleCallbacks.cs
@@ -463,6 +463,71 @@ package Support_VehicleCallbacks
 
     }
 
+    //Add small check to disable vehicles from being pushed.
+    function Vehicle::onActivate(%vehicle, %activatingObj, %activatingClient, %pos, %vec)
+    {
+        if(%vehicle.Datablock.cantPush == true)
+        {
+            return;
+        }
+        else if(VectorLen(%vehicle.getVelocity()) > 2)
+        {
+            return;
+        }
+        
+        %client = %activatingClient;
+        if(!isObject(%client))
+        {
+            return;
+        }
+
+        %player = %activatingObj;
+        if(!isObject(%player))
+        {
+            return;
+        }
+
+        %doFlip = 0;
+
+        if(isObject(%vehicle.spawnBrick))
+        {
+            %vehicleOwner = findClientByBL_ID(%vehicle.spawnBrick.getGroup().bl_id);
+        }
+        else
+        {
+            %vehicleOwner = 0;
+        }
+
+        if(isObject(%vehicleOwner))
+        {
+            if(getTrustLevel(%player, %vehicle) >= $TrustLevel::VehicleTurnover)
+            {
+                %doFlip = 1;
+            }
+        }
+        else
+        {
+            %doFlip = 1;
+        }
+
+        %minigameCanUse = miniGameCanUse(%player, %vehicle);
+        if(%minigameCanUse != -1)
+        {
+            %doFlip = %minigameCanUse;
+        }
+        
+        if(%doFlip)
+        {
+            %impulse = VectorNormalize(%vec);
+            %impulse = VectorAdd(%impulse, "0 0 1");
+            %impulse = VectorNormalize(%impulse);
+            %force = %vehicle.getDataBlock().mass * 5;
+            %scaleRatio = getWord(%player.getScale(), 2) / getWord(%vehicle.getScale(), 2);
+            %force *= %scaleRatio;
+            %impulse = VectorScale(%impulse, %force);
+            %vehicle.applyImpulse(%pos, %impulse);
+        }
+}
 };
 activatePackage(Support_VehicleCallbacks);
 
